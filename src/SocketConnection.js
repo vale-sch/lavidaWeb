c6ebabb45fd67df0f8c1a7c8712711c84d067035
vale-sch <vali.vogt@live.de>
2023-12-25 14:41:42 +0100

    fix error from last push

--- src/SocketConnection.js
+++ src/SocketConnection.js
@@ -3,6 +3,6 @@ export let socket;
 export let requestInfoObj;
 export function connectClientID(userID) {
     //@ts-ignore
-    socket = io("wss://lavidasocket.onrender.com "); //ws://localhost:8080
+    socket = io("wss://lavidasocket.onrender.com"); //ws://localhost:8080
     socket.emit("onconnect", JSON.stringify(userID));
 }
