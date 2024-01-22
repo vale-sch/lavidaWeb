ca7e1592eabc706951f17e8cda33c4dffc35385e
vale-sch <vali.vogt@live.de>
2024-01-22 17:07:33 +0100

    added loading div

--- src/SocketConnection.js
+++ src/SocketConnection.js
@@ -3,6 +3,6 @@ export let socket;
 export let requestInfoObj;
 export function connectClientID(userID) {
     //@ts-ignore
-    socket = io("ws://localhost:8080"); //wss://lavidasocket.onrender.com
+    socket = io("wss://lavidasocket.onrender.com"); //ws://localhost:8080
     socket.emit("onconnect", JSON.stringify(userID));
 }
